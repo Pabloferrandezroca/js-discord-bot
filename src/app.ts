ab4768c7a161c809b6ec44f4217a2e628fae5c56
Pablo Ferrandez Roca <pferrandezroca2@gmail.com>
2025-04-03 11:39:20 +0200

    añidida IA

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import 'dotenv/config'
 import { Client, GatewayIntentBits, Events } from 'discord.js'
+import { main } from './gemini';
 
 // The Client and Intents are destructured from discord.js, since it exports an object by default. Read up on destructuring here https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
 const client = new Client({
@@ -10,10 +11,11 @@ client.on(Events.ClientReady, readyClient => {
   console.log(`Logged in as ${readyClient.user.tag}!`);
 });
 
-client.on(Events.MessageCreate, message => {
-  console.log("ha escrito el espabilao")
-  if (message.content == "ping") {
-    message.channel.send("pong!");
+client.on(Events.MessageCreate, async message => {
+  if (message.content.startsWith('!')){
+    let pregunta = message.content.substring(1);
+    let respuesta = main(pregunta);
+    message.channel.send(await respuesta);
   }
 });
 
---
+++ src/gemini.ts
@@ -0,0 +1,32 @@
+// src/app.ts
+import { GoogleGenAI } from "@google/genai";
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+import 'dotenv/config'
+const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+const ai = new GoogleGenAI({ apiKey: process.env.API_KEY });
+let history: Array<any> = [];
+ export async function main(mensaje: string):Promise<string> {
+  history.push({
+    role: "user",
+    parts: [{ text: mensaje }],
+  });
+  const response = await ai.models.generateContent({
+    model: "gemini-2.0-flash",
+    contents: mensaje
+  });
+  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
+  const chat = model.startChat({
+    history: history,
+    generationConfig: {
+      maxOutputTokens: 100,
+    },
+  });
+  const result = await chat.sendMessage(mensaje);
+  const respuesta = await result.response;
+  const text = respuesta.text();
+  history.push({
+    role: "model",
+    parts: [{ text: text }],
+  });
+  return text as string;
+}
